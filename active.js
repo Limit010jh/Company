248fdad1895c6c3a502062d9be755065b9f8dba3
Limit010jh <adamboujidi001@gmail.com>
2026-05-28 21:21:13 +0100

    Update active.js

--- active.js
+++ active.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
       };
 
       try {
-        const response = await fetch('https://limit22274.app.n8n.cloud/webhook/75350ef9-5399-4bb3-995c-56ebd8ab7244', {
+        const response = await fetch('https://limit22274.app.n8n.cloud/webhook-test/75350ef9-5399-4bb3-995c-56ebd8ab7244', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(formData)
